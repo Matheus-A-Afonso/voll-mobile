1c586f8302ef7cc00fe686ad153eabcd745f9687
Matheus Afonso <godsmatheus@gmai.com>
2024-06-05 03:27:14 -0300

    Seções de cadastro

--- src/cadastro.tsx
+++ src/cadastro.tsx
@@ -1,55 +1,74 @@
-import {VStack, Image, Text, Box, FormControl, Input, Button, Link,}from 'native-base'
+import { VStack, Image, Text, Box, }from 'native-base'
 import { TouchableOpacity } from 'react-native';
 import Logo from './assets/Logo.png';
 import { Titulo } from './componentes/titulo';
 import { EntradaTexto } from './componentes/EntradaTexto';
 import { Botao } from './componentes/Botao';
+import { useState } from 'react';
 export default function Login() {
   
+  const [numSecao, setNumScao] = useState (0);
   const secoes = [
     {
       id: 1,
-      titulo: 'insira seus dados',
+      titulo: 'Insira seus dados',
       entradaTexto: [
         {
-          id: 1,
+          id: 2,
           label: 'Nome',
           placeholder: 'Insira seu nome'
         },
         {
-          id: 2,
+          id: 3,
           label: 'Email',
           placeholder: 'Insira seu email'
         },
         {
-          id: 2,
+          id: 4,
           label: 'Senha',
           placeholder: 'Insira sua senha'
         },
         {
-          id: 2,
+          id: 5,
           label: 'Repita a senha',
           placeholder: 'Insira sua senha'
         },
       ]
+    },
+    {
+      id: 2,
+      titulo: 'Agora, mais alguns dados sobre você',
+      entradaTexto: [
+        {
+          id: 1,
+          label: 'CEP',
+          placeholder: 'Digite seu CEP'
+        }
+      ]
     }
   ]
-
+  function avancarSecao(){
+    setNumScao(numSecao+1)
+  }
   return (
      
     <VStack flex={1} alignItems= "center" justifyContent="top" p={5}>
       <Image source={Logo} alt="Logo Voll" />
 
-     
+        <Titulo>
+          {secoes[numSecao].titulo}
+        </Titulo>     
         <Box>
           {
-            secoes [0].entradaTexto.map(entrada => {
+            secoes [numSecao].entradaTexto.map(entrada => {
               return <EntradaTexto label={entrada.label} placeholder={entrada.placeholder} />
             })
           }
 
         </Box>
-          <Botao>Avançar</Botao>
+          <Botao onPress={() => avancarSecao()}>
+            Avançar
+        </Botao>
     </VStack>
   );
 }
--- src/componentes/titulo.tsx
+++ src/componentes/titulo.tsx
@@ -1,17 +1,21 @@
-import {Text} from 'native-base'
+import { Text, ITextProps } from "native-base"
+import { ReactNode } from "react"
 
+interface TituloProps extends ITextProps {
+  children: ReactNode
+}
 
-export function Titulo ({children, ...rest}){
-    return(
-        <Text
-        fontSize="xl"
-        fontWeight="black"
+export function Titulo({ children, ...rest }: TituloProps){
+  return (
+    <Text
+        fontSize="2xl"
+        fontWeight="bold"
         color="gray.500"
-        textAlign= 'center'
-        marginTop={5}
+        textAlign="center"
+        mt={5}
         {...rest}
       >
         {children}
       </Text>
-    )
+  )
 }
